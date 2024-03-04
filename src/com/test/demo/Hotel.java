2fa2dd22b6f57d39ee81d924851eec00749e20f9
ajohnmar <ajohnmar@LIN79001362.corp.capgemini.com>
2024-03-04 12:01:39 +0530

    commit

--- src/com/test/demo/Hotel.java
+++ src/com/test/demo/Hotel.java
@@ -2,36 +2,91 @@ package com.test.demo;
 
 import java.time.Duration;
 
+import org.junit.Assert;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
+import com.test.utils.DriverUtils;
+
+
 public class Hotel {
+
+	static WebDriver driver;
+	static DriverUtils util;
+	static String noBtn = "//span[@class='mat-button-wrapper' and text()='No']";
+	static String closeBtn = "//div[@class='closeBtnWrapper']";
+	static String ballonBtn = "//button[@class='btn btn-default balloonButton']";
+
+	static String countryDropDown = "//select[@name='country']";
+	static String india = "IN";
+	static String cityDropDown = "//select[@name='city']";
+	static String chennai = "46699";
+	static String buildingDropDown = "//select[@name='building']";
+	static String buildingName = "IN036OF01";
+	static String floorDropDown = "//select[@name='floor']";
+	static String floorNumber = "38";
+
+	static String findBuilding = "//label[@class='link serachOfficeBtn']";
+	static String enterName = "//input[@class='form-control ng-pristine ng-valid ng-touched']";
+	static String inputVal = "PCT";
+	static String closeBtn2 = "//button[@class='btn btn-secondary']";
+	static String dataSelectionBtn = "//td[@data-day='03/01/2024']";
+	static String bookBtn = "//button[@type='button' and text()='Book']";
+	static String getTextmsg = "//p[@class='m-0 ng-star-inserted']";
+	static String getErrorMsg=" fulfil The system cannot";
+
 	public static void main(String[] args) {
+		OpenApplication();
+		CloseInitialPrompts();
+		SelectSeatBookingOptions();
+		//FindBuildingName();
+		GetText();
+		driver.quit();
+	}
+
+	private static WebDriver OpenApplication() {
 		System.setProperty("WebDriver.chrome.driver", "C:/Users/AJOHNMAR/Downloads/chromedriver.exe");
-		WebDriver driver=new ChromeDriver();
+		driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 		driver.get("https://hoteling.capgemini.com/reservationContent/newReservation");
-		WebElement wb=driver.findElement(By.xpath("//span[@class='mat-button-wrapper' and text()='No']"));
-		wb.click();
-		//driver.switchTo().alert().dismiss();
-		WebElement wbd=driver.findElement(By.xpath("//div[@class='closeBtnWrapper']"));
-		wbd.click();
-		WebElement ws=driver.findElement(By.xpath("//button[@class='btn btn-default balloonButton']"));
-		ws.click();
-		Select cls=new Select(driver.findElement(By.xpath("//select[@name='country']")));
-		cls.selectByValue("IN");
-		Select cls1=new Select(driver.findElement(By.xpath("//select[@name='city']")));
-		cls1.selectByValue("46699");
-		Select cls2=new Select(driver.findElement(By.xpath("//select[@name='building']")));
-		cls2.selectByVisibleText("PCT");
-		Select cls3=new Select(driver.findElement(By.xpath("//select[@name='floor']")));
-		cls3.selectByValue("38");
-		WebElement ws1=driver.findElement(By.xpath("//td[@class='day disabled']//following::td[@class='day new']"));
-		ws1.click();
+		util = new DriverUtils(driver);
+		return driver;
+	}
+
+	private static void CloseInitialPrompts() {
+		util.Click(noBtn);
+		util.Click(closeBtn);
+		util.Click(ballonBtn);
+
+	}
+
+//	private static void FindBuildingName() {
+//		util.Click(findBuilding);
+//		util.Sendkeys(enterName, inputVal);
+//		util.Click(closeBtn2);
+//
+//	}
+
+	private static void SelectSeatBookingOptions() {
+		util.Select(countryDropDown, india);
+		util.Select(cityDropDown, chennai);
+		util.Select(buildingDropDown, buildingName);
+		util.Select(floorDropDown, floorNumber);
+		util.Click(dataSelectionBtn);
+		util.Click(bookBtn);
+	}
+	
+	private static void GetText() {
+		System.out.println(util.GetText(getTextmsg));
+		//assert getErrorMsg.equals(util.GetText(getTextmsg));
+		//Assert.assertEquals(util.GetText(getTextmsg),"Please enter your Email ID");
+		Assert.assertEquals(getErrorMsg,util.GetText(getTextmsg));
 		
+
 	}
 
 }
